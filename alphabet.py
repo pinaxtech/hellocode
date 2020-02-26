cdf1b41fef0f0ffe66f80e70841a1ecd4a9d05ec
pinaxtech <adityaroy62613@gmail.com>
2020-02-26 11:44:09 +0530

    new change

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+n = input("Enter a character: ")
+
+if(n=='A' or n=='a' or n=='E' or n =='e' or n=='I'
+ or n=='i' or n=='O' or n=='o' or n=='U' or n=='u'):
+    print(n, "is a Vowel")
+else:
+    print(n, "is a Consonant")
